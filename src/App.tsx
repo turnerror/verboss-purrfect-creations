26d5baf1a8168bd224f271b8f7dd77e7ecce5334
turnerror <christianturner.dev@gmail.com>
2023-01-19 20:53:58 +0000

    calculated: monthly, total, inprogress orders and total revenue on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react';
-import { getTotal } from './api';
+import { getData } from './api';
 import './App.css';
 
 function App() {
 
   const [totalOrders, setTotalOrders] = useState(0);
-
-  getTotal().then((total => console.log({total})))
+  const [monthyTotal, setMonthlyTotal] = useState(0);
+  const [inProgressTotal, setInProgressTotal] = useState(0);
+  const [revenue, setRevenue] = useState(0);
 
   useEffect(() => {
     (async () => {
-      setTotalOrders (await getTotal());
+      // setTotalOrders (await getTotal());
+      const {total, monthlyTotal, inProgressTotal, revenue} = await getData();
+      setTotalOrders(total);
+      setMonthlyTotal(monthlyTotal);
+      setInProgressTotal(inProgressTotal);
+      setRevenue(revenue);
     })()
   }, []);
   
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Total Orders {totalOrders}</h2>
+        <h1>Purrfect Creations Dashboard!!!</h1>
+        <h2>Total Orders:  {totalOrders}</h2>
+        <h2>Total Orders this Month: {monthyTotal}</h2>
+        <h2>Number of orders in progress: {inProgressTotal}</h2>
+        <h2>Total revenue: ${revenue}</h2>
       </header>
     </div>
   );
--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 
 const base = "https://api.airtable.com/v0/app8wLQrrIMrnn673";
 
@@ -8,34 +9,113 @@ const authHeader = {
         Authorization: "Bearer " + token
     };
 
-export const getTotal = async () => {
-    const response = await axios.get(base + "/orders", {
-    headers: {
-        Authorization: "Bearer " + token
-    },
+
+type OrderFields = {
+order_id: number,
+order_placed: string,
+product_name: string,
+price: number,
+first_name: string,
+last_name: string,
+address: string,
+email: string,
+order_status: "in_progress" | "cancelled" | "placed" | "shipped" 
+}
+
+type Order = {id: string, createdTime: string, fields: OrderFields};
+
+type OrderResponseSchema = {
+    records: Order[],
+    offset: string,
+
+}
+
+export const getData = async () => {
+
+    const sort =  [{field: "order_placed", direction: "desc"}];
+    const response = await axios.post(base + "/orders/listRecords", {
+        sort
+    },{
+    headers: authHeader
 });
 
-    let orders = response.data.records;
+    let orders: Order[] = response.data.records;
 
     let nextPageOffset = response.data.offset;
 
     while(nextPageOffset) {
-        const response = await axios.get(base + "/orders?offset=" + nextPageOffset, {headers: authHeader} );
+        const response = await axios.post<OrderResponseSchema>(base + "/orders/listRecords",{sort, offset: nextPageOffset}, {headers: authHeader} );
 
         nextPageOffset = response.data.offset;
 
         orders = [...orders, ...response.data.records];
 
-        console.log({nOrders: orders.length});
     }
 
 
 
     console.log({records: response.data.records, response});
-    return orders.length;
+
+    const oneMonthAgo = moment().subtract(1, "M");
+
+    const thisMonthOrders = orders.filter((order) => {
+        const timeOfOrder = moment(order.fields.order_placed, "YYYY-MM-DD");
+
+        return timeOfOrder.isSameOrAfter(oneMonthAgo);
+    });
+
+    const inProgressOrders = orders.filter((order) => {
+        return order.fields.order_status === "in_progress";
+    })
+
+    let revenue = 0;
+
+    for (const order of orders) {
+        if (order.fields.order_status !== "cancelled") {
+            revenue += order.fields.price;
+        }
+    }
+
+    return {total: orders.length, monthlyTotal: thisMonthOrders.length, last5: orders.slice(0, 5), inProgressTotal: inProgressOrders.length, revenue: +revenue.toFixed(2)};
 }
+
 
 
 export const getTotalThisMonth = async () => {
+    // const latestQueryParam = 'sort=[{field: "order_placed", direction: "desc"}]';
 
+    // const oneMonthAgo = moment().subtract(1, "M").format("MM/DD/YYYY");
+    const oneMonthAgo = "27/9/2022";
+
+    console.log({oneMonthAgo});
+
+    const lastMonthOnlyFilter = `IS_AFTER({order_placed}, "${oneMonthAgo}")`
+    const response = await axios.get(`${base}/orders?filterByFormula=${lastMonthOnlyFilter}`, {
+    headers: {
+        Authorization: "Bearer " + token
+    },
+});
+
+console.log({response});
+
+
+//     let orders = response.data.records;
+
+//     let nextPageOffset = response.data.offset;
+
+//     while(nextPageOffset) {
+//         const response = await axios.get(`${base}/orders?${latestQueryParam}&offset=${nextPageOffset}`, {headers: authHeader} );
+
+//         nextPageOffset = response.data.offset;
+
+//         orders = [...orders, ...response.data.records];
+
+//         console.log({nOrders: orders.length});
+//     }
+
+
+
+//     console.log({records: response.data.records, response});
+//     return orders.length;
 }
+
